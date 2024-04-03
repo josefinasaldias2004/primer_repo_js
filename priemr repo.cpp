8bd153332f0c3ddbdddb4d11f007098e1dd0dac9
Josefina_Saldias <josefinabateria@gmail.com>
2024-04-03 11:41:20 -0300

    calculadora actualizada

--- priemr repo.cpp
+++ priemr repo.cpp
@@ -2,14 +2,47 @@
 using namespace std;
 int main ()
 {
- int Numero_1 , Numero_2 , suma;
+ int Numero_1 , Numero_2 , suma,resta,  menu;
 
     cout<< "ingrese el primer numero" << endl;
      cin>> Numero_1;
      cout<<"ingrese numero 2" << endl;
      cin>> Numero_2;
      suma = Numero_1 + Numero_2;
+     resta = Numero_1 - Numero_2;
+
+ cout<< "______MENU_______"<< endl;
+     cout<< "que desea hacer"<<endl;
+     cout<< "1. Sumar los dos numeros"<< endl;
+     cout<< "2. restar los dos numeros" <<endl;
+     cout << "3. comparar de dos numeros" <<endl;
+     cin>> menu;
+
+
+
+     switch (menu)
+
+     {
+
+     case 1 :
+{
+
+
+
      cout<< "la suma es de "<<suma<< endl;
+      break;
+}
+     case 2:
+         {
+
+
+        cout<< "la resta es de " <<resta<<endl;
+
+        break;
+         }
+     case 3:
+{
+
 
      if (Numero_1 < Numero_2)
      {
@@ -21,7 +54,11 @@ int main ()
 
         cout << "el segundo numero ingresado es menor q el primero"<< endl;
      }
-     return  0 ;
+
+     break;
+     return 0;
+}
+     }
 }
 
 
